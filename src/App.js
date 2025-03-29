da8c0929d421e1c9e83cae2d21a8d5dbed857170
Aayush Vaishnav <vaishnavaayush79@gmail.com>
2025-03-30 01:40:53 +0530

    Fix GitHub Pages issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LogInPage from "./components/LogInPage";
 import SignUpPage from "./components/SignUpPage";
 import HomePage from "./components/HomePage";
